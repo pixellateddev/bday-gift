6d18fdb004822b2a86ab4487d5b2d7109a0a9426
Himanshu Sagar <h_sagar@apple.com>
2022-11-29 15:07:23 +0530

    updated styles

--- pages/gifts/[id].tsx
+++ pages/gifts/[id].tsx
@@ -9,7 +9,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 
-import bdayPic from '../../public/nick.jpg'
 import {sanityClient, urlFor} from '../../sanity'
 
 
@@ -75,53 +74,22 @@ export default function Home({bday}: Props ) {
 
             <div className='header'>
                 <h1>Today is {bday.name}&apos;s birthday 🎉</h1>
-                <Image className='bday-img' alt='' src={urlFor(bday.image).url()} width={150} height={150}/>
+                <Image className='bday-img' alt={`{bday.name}'s Pic`} src={urlFor(bday.image).url()} width={150} height={150}/>
                 <h2 className='bday-age'>{getAge(bday.dob)} years old</h2>
                 <h4 className='bday-date'>{bday.dob}</h4>
             </div>
 
             {bday.gifts.map((gift, index) => (
-                <div key={index} className="gift-section">
-                    <h2 className="gift-title">{gift.description}</h2>
-                    {index === 0 && <h3 className="gift-hint">(Hover over the gift)</h3>}
-                    <div className="gift-img" css={({
+                <div key={index} className='gift-section'>
+                    <h2 className='gift-title'>{gift.description}</h2>
+                    {index === 0 && <h3 className='gift-hint'>(Hover over the gift)</h3>}
+                    <div className='gift-img' css={({
                         '&:hover': {
                             backgroundImage: `url("${urlFor(gift.gift).url()}")`
                         }
                     })}></div>
                 </div>  
             ))}
-
-            {/* <div className="gift-section">
-                    <h2 className="gift-title">Here&apos;s how happy I am for you today 🥳</h2>
-                    <h3 className="gift-hint">(Hover over the gift)</h3>
-                    <div className="gift-img" id="gift-img-happy"></div>
-            </div>
-                
-            <div className="gift-section">
-                <h2 className="gift-title">How people react when you enter the room 😍</h2>
-                <div className="gift-img" id="gift-img-hot"></div>
-            </div>
-            
-            <div className="gift-section">
-                <h2 className="gift-title">If I had to describe you with ONE word 👇</h2>
-                <div className="gift-img" id="gift-img-genius"></div>
-            </div>
-            
-            <div className="gift-section">
-                <h2 className="gift-title">The only person as badass as you 💪</h2>
-                <div className="gift-img" id="gift-img-badass"></div>
-            </div>
-            
-            <div className="gift-section">
-                <h2 className="gift-title">This one&apos;s for you, my friend 🥂</h2>
-                <div className="gift-img" id="gift-img-cheers"></div>
-            </div>
-
-            <div className="gift-section">
-                <h2 className="gift-title">Have a blast 💥</h2>
-                <div className="gift-img" id="gift-img-blast"></div>
-            </div> */}
         </div>
     )
 }
