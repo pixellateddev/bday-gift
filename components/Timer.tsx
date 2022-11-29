64923466cac0b541e72946f308d23fca76f9f7f5
Himanshu Sagar <h_sagar@apple.com>
2022-11-29 22:55:50 +0530

    fixed round off issue

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -13,12 +13,13 @@ const Timer: FC<Props> = ({till, done}) => {
         done()
     }
     let n = diff
-    const days = Math.round(n / (24 * 3600))
+    const days = Math.floor(n / (24 * 3600))
     n = n % (24 * 3600);
-    const hours = Math.round(n / 3600)
+    const hours = Math.floor(n / 3600)
     n = n % 3600
-    const minutes = Math.round(n / 60)  
+    const minutes = Math.floor(n / 60)  
     const seconds = n % 60
+
     useEffect(() => {
         const interval = setInterval(() => {
             setDiff(prevDiff => prevDiff - 1)
@@ -35,12 +36,16 @@ const Timer: FC<Props> = ({till, done}) => {
             <div className="timer">
                 <h2>You are here early</h2>
                 <h3>Come back in</h3>
-                <p className="time">
-                    {days !== 0 && <span>{days > 1 ? `${days} Days` : `${days} Day`}, </span>}
-                    {(days + hours) !== 0 && <span>{hours > 1 ? `${hours} Hours` : `${hours} Hour`}, </span>}
-                    {(days + hours + minutes) !== 0 && <span>{minutes > 1 ? `${minutes} Minutes` : `${minutes} Minute`}, </span>}
-                    {(days + hours + minutes + seconds) !== 0 && <span>{seconds > 1 ? `${seconds} Seconds` : `${seconds} Second`}</span>}
-                </p>
+                <div className="time">
+                    <p>
+                        {days !== 0 && <span>{days > 1 ? `${days} Days` : `${days} Day`}, </span>}
+                        {(days + hours) !== 0 && <span>{hours > 1 ? `${hours} Hours` : `${hours} Hour`}, </span>}
+                    </p>
+                    <p className="second">
+                        {(days + hours + minutes) !== 0 && <span>{minutes > 1 ? `${minutes} Minutes` : `${minutes} Minute`}, </span>}
+                        {(days + hours + minutes + seconds) !== 0 && <span>{seconds > 1 ? `${seconds} Seconds` : `${seconds} Second`}</span>}
+                    </p>
+                </div>
 
                 <h3>To see your gift 🎁</h3>
             </div>
